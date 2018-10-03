7887172d2808c4370052fb8a136aefd27ddc7061
Garet C. Green <garetgreen1996@gmail.com>
2018-10-03 12:58:16 -0500

    Add resources to the default layout

--- Layouts/Default.php
+++ Layouts/Default.php
@@ -1,23 +1,32 @@
 <?php
+	require_once(__DIR__ . "/../Classes/Internals.php");
+
 	global $headContents;
 	global $bodyContents;
 ?>
 <html lang="en">
 	<head>
 		<link rel="stylesheet" href="/Assets/styles/theme.css" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script defer src="/Assets/js/bootstrap.bundle.min.js"></script>
+		<script defer src="/Assets/js/image-uploader.min.js"></script>
 		<?= $headContents; ?>
 	</head>
-	<body>
-		<div id="header">
-			<?php include(__DIR__  . "/../Partials/header.php"); ?>
+	<body class="">
+		<div class="">
+			<div id="header">
+				<?php include(__DIR__  . "/../Partials/header.php"); ?>
+			</div>
+			<div id="navigation">
+				<?php include(__DIR__  . "/../Partials/navigation.php"); ?>
+			</div>
+			<div id="page" class="mx-auto">
+				<div class="container-fluid my-4">
+					<?= $bodyContents; ?>
+				</div>
+			</div>
 		</div>
-		<div id="navigation">
-		<?php include(__DIR__  . "/../Partials/navigation.php"); ?>
-		</div>
-		<div id="page">
-			<?= $bodyContents; ?>
-		</div>
+		<?php include(__DIR__  . "/../Partials/footer.php"); ?>
 	</body>
 </html>
