0a1ab278e92581d2e3b1640f866f3c31e4cc9e70
Garet C. Green <garetgreen1996@gmail.com>
2018-10-03 12:57:46 -0500

    Add heading to the header

--- Partials/header.php
+++ Partials/header.php
@@ -1,3 +1,3 @@
-<div class="bg-primary py-3" id="inner-header">
-
+<div class="bg-primary py-4" id="inner-header">
+	<h2 id="header-title" class="text-white text-center">Queryable Stock-Photo Database</h2>
 </div>
